69b34222da5af8fb0c242c415ab7e1afde65258e
csbetway <csbetway@gmail.com>
2021-05-24 23:19:38 +0700

    add bac

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,57 +6,57 @@ import Hero from "../components/hero";
 import Layout from "../components/layout";
 import ArticlePreview from "../components/article-preview";
 import styled from "styled-components";
-// import GIF_1 from "../../static/images/Gif-1.gif";
-// import GIF_2 from "../../static/images/Gif-2.gif";
-// import GIF_3 from "../../static/images/Gif-3.gif";
-// import GIF_LINE from "../../static/images/Gif-line.gif";
-// import GIF_REGISTER from "../../static/images/Gif-register.gif";
-// import { Link } from "gatsby";
-// import { MEDIA } from "../utils/stylesPageLayout";
+import GIF_1 from "../../static/images/Gif-1.gif";
+import GIF_2 from "../../static/images/Gif-2.gif";
+import GIF_3 from "../../static/images/Gif-3.gif";
+import GIF_LINE from "../../static/images/Gif-line.gif";
+import GIF_REGISTER from "../../static/images/Gif-register.gif";
+import { Link } from "gatsby";
+import { MEDIA } from "../utils/stylesPageLayout";
 
-// const Wrapper = styled.div`
-//   position: relative;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   width: 100%;
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
 
-//   background-color: black;
-//   padding: 40px 0;
-// `;
+  background-color: black;
+  padding: 40px 0;
+`;
 
-// const ImageWrapper = styled.div`
-//   margin: 0 40px 40px;
+const ImageWrapper = styled.div`
+  margin: 0 40px 40px;
 
-//   ${MEDIA.DESKTOP`
-//     width: 728px;
-//   `}
-// `;
+  ${MEDIA.DESKTOP`
+    width: 728px;
+  `}
+`;
 
-// const LineWrapper = styled.div`
-//   margin: 0 40px 40px;
-// `;
+const LineWrapper = styled.div`
+  margin: 0 40px 40px;
+`;
 
-// const Image = styled.img`
-//   width: 100%;
-//   height: auto;
-// `;
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+`;
 
 const MainWrapper = styled.div`
   position: relative;
 `;
 
-// const Background = styled.div`
-//   position: absolute;
-//   top: 0;
-//   bottom: 0;
-//   left: 0;
-//   right: 0;
-//   background-color: black;
-//   width: 100%;
-//   z-index: 1;
-//   opacity: 0;
-// `;
+const Background = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: black;
+  width: 100%;
+  z-index: 1;
+  opacity: 0;
+`;
 
 class RootIndex extends React.Component {
   render() {
@@ -65,12 +65,12 @@ class RootIndex extends React.Component {
     const [author] = get(this, "props.data.allContentfulPerson.edges");
     const metaDescription = "ปุ๋ยบาคา ปุ๋ยดีของไทย ไว้ใจได้ทุกไร่ร่า";
     const keywords = ["ปุ๋ยบาคา"];
-    // const LINK_URL = "https://cs1baccarat.com/";
-    // const LINE_URL = "line://ti/p/@CSBETWAY";
+    const LINK_URL = "https://cs1baccarat.com/";
+    const LINE_URL = "line://ti/p/@CSBETWAY";
 
     return (
       <div>
-        {/* <Wrapper>
+        <Wrapper>
           <ImageWrapper>
             <Link to={LINK_URL}>
               <Image src={GIF_1} />
@@ -96,7 +96,7 @@ class RootIndex extends React.Component {
               <Image src={GIF_LINE} />
             </a>
           </LineWrapper>
-        </Wrapper> */}
+        </Wrapper>
         <MainWrapper>
           <Layout location={this.props.location}>
             <div style={{ background: "#fff" }}>
@@ -145,7 +145,7 @@ class RootIndex extends React.Component {
               </div>
             </div>
           </Layout>
-          {/* <Background /> */}
+          <Background />
         </MainWrapper>
       </div>
     );
